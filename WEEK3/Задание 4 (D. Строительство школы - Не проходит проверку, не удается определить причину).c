24745cfcc29ddacfd0d094f16512a841cc2bfe9e
kuzevan52025 <kuzevan52025@gmail.com>
2022-02-28 12:50:32 +0300

    WEEK3\Задание 2, 3
    WEEK3\Задание 3 (Вывод ФИО)
    WEEK3\Задание 4 (D. Строительство школы - Не проходит проверку, не удается определить причину)

---
+++ WEEK3/Задание 4 (D. Строительство школы - Не проходит проверку, не удается определить причину).c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() 
+{
+    int students;
+    float SumCoordinates;
+    int CoordinatHouse;
+    int CoordinatSchool;
+    float average;
+    
+    int MaximumStudents[100000];
+    
+    bool ConditionCompleted = true;
+        while (ConditionCompleted)
+        {
+            scanf("%d", &students);
+                if (students > 0 && students < 100001)
+                    ConditionCompleted = false;
+        }
+    
+    for (int i = 0; i < students; i++)
+    {
+        scanf("%d", &CoordinatHouse);
+        SumCoordinates = SumCoordinates + CoordinatHouse;
+    }
+    
+    average = (float) SumCoordinates / students;
+    CoordinatHouse = average;
+    
+    if (average - (int) average > 0.5)
+    {
+        CoordinatSchool = CoordinatSchool ++;
+    }
+    
+    printf("%d", CoordinatHouse);
+    
+    return 0;
+}
