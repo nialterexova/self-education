d241e3af6dd484b9fecb263db0134412568a9f09
NataliaChernyshova <nachernyshova.hse@gmail.com>
2022-03-13 17:01:26 +0300

    Исправления
    Исправлены подсчеты (добавлена табуляция, поправлен вывод каждого слова на отдельной строке, пропускаю вывод нескольких пробелов и табуляций); исправлены инициалы (учтен ввод нескольких пробелов)

--- Week 3/Подсчеты/Подсчеты.c
+++ Week 3/Подсчеты/Подсчеты.c
@@ -6,26 +6,27 @@ void print_count_symbols(char separator){
   int count_words = 0;
   int count_lines = 0;
   while (symbol != separator) {
-    if (symbol == '\n'){
-      count_lines += 1;
+    while (symbol != '\n'){
+      while (symbol != ' ' && symbol != '\t' && symbol != '\n') {
+        printf("%c", symbol);
+        count_symbols += 1; 
+        symbol = getchar();
       }
-    if (symbol == ' ' || symbol == '\n') {
       count_words += 1;
-    }
-    if (symbol != '\n' && symbol != ' '){
-      printf("%c", symbol);
-      }
-    else{
       printf("\n");
+      while (symbol == ' ' || symbol == '\t'){
+        count_symbols += 1;
+        symbol = getchar();
+      }
     }
-    symbol = getchar();
+    count_lines += 1;
     count_symbols += 1;
-  }
+    symbol = getchar();
+  }  
   printf("\nКоличество символов: %i\n", count_symbols);
   printf("Количество слов: %i\n", count_words);
   printf("Количество строк: %i\n", count_lines);
 }
-  
 int main() {
   print_count_symbols(EOF);
   return 0;
